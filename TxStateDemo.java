3d173b44f4dec7aa9edbe28966668e8e4fe52898
Beau Welder <beauwelder@gmail.com>
2019-11-08 08:44:12 -0600

    New change

--- TxStateDemo.java
+++ TxStateDemo.java
@@ -3,6 +3,7 @@ public class TxStateDemo {
 	public static void main(String... args) {
 
 		String greeting = "This world is awesome!";
+		String newGreeting = "Can I push now?";
 		System.out.println(greeting);
 	}
 }
